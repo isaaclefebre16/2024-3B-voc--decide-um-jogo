07ef0f8938c56ac1d654e0cd44e212135641a659
isaaclefebre16 <135345043+isaaclefebre16@users.noreply.github.com>
2024-07-04 08:35:55 -0300

    Aula 04-07-2024

---
+++ main.js
@@ -0,0 +1,26 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
+const caixaAlternativa = document.querySelector('.caixa-alternativas');
+const caixaResultado = document.querySelector('.caixa-resultado');
+const textoResultado = document.querySelector('.texto-resultado');
+const perguntas = [
+    {
+        enunciado: "pergunta 1",
+        alternativa: [
+            "Alternativa 1"
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativa: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
+]
+
+
+
+
+let atual
